dab97c0d113ef4097a85ea9b7d67441ebfe8a28b
Achref LAABIDI <abidi.achref95@gmail.com>
2023-06-20 20:13:39 +0100

    models done

---
+++ models/Post.model.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  thread: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Thread',
+    required: true
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ],
+  comments: [
+    {
+      content: {
+        type: String,
+        required: true
+      },
+      author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('Post', postSchema);
