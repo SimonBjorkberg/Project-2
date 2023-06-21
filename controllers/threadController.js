68d52e729bebad0ffc1cd5e84e886e05d4287f85
Achref LAABIDI <abidi.achref95@gmail.com>
2023-06-21 14:38:34 +0100

    created threadController and route

---
+++ controllers/threadController.js
@@ -0,0 +1,68 @@
+const Thread = require('../models/thread.model');
+
+// ###########
+// function that creates a new thread
+// ###########
+
+exports.createThread = async (req, res) => {
+  try {
+    const { title, content, author } = req.body
+    const newThread = new Thread({ title, content, author })
+    const savedThread = await newThread.save()
+    res.status(201).json(savedThread)
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to create a new thread' })
+  }
+}
+
+// ###########
+// function that gets a thread by its ID
+// ###########
+
+exports.getThread = async (req, res) => {
+  try {
+    const { threadId } = req.params
+    const thread = await Thread.findById(threadId)
+    if (!thread) {
+      return res.status(404).json({ error: 'Thread not found' })
+    }
+    res.json(thread)
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to get thread' })
+  }
+}
+
+// ###########
+// function that updates thread by ID
+// ###########
+
+exports.updateThread = async (req, res) => {
+  try {
+    const { threadId } = req.params
+    const { title, content } = req.body
+    const updatedThread = await Thread.findByIdAndUpdate(threadId, { title, content }, { new: true })
+    if (!updatedThread) {
+        return res.status(404).json({ error: 'Thread not found' })
+      }
+      res.json(updatedThread)
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to update thread' })
+    }
+  }
+
+// ###########
+// function that deletes thread by ID
+// ###########
+
+exports.deleteThread = async (req, res) => {
+  try {
+    const { threadId } = req.params
+    const deletedThread = await Thread.findByIdAndDelete(threadId)
+    if (!deletedThread) {
+      return res.status(404).json({ error: 'Thread not found' })
+    }
+    res.json({ message: 'Thread deleted successfully' })
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to delete thread' })
+  }
+}
