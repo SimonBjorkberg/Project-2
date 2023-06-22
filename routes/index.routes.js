dd6f5c7bae10fa5cffb74e02d86c396dc2378b21
Simon Björkberg <127056123+SimonBjorkberg@users.noreply.github.com>
2023-06-22 13:21:41 +0200

    u can now create and see all threads, including the author

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Thread = require("../models/thread.model");
+const User = require("../models/User.model");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index", { userInSession: req.session.currentUser });
+  Thread.find({})
+    .populate("author")
+    .then((thread) => {
+      res.render("index", { userInSession: req.session.currentUser, thread });
+      console.log(thread);
+    })
+    .catch((err) => console.log("err", err));
 });
 
 module.exports = router;
