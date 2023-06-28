de5036a751d7ab17c90b28b75c3e903eb4f9b82b
Simon Björkberg <127056123+SimonBjorkberg@users.noreply.github.com>
2023-06-28 03:36:16 +0200

    changed the look of the create thread form

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,20 +3,19 @@ const User = require('../models/User.model');
 const getAllUsers = async (req, res) => {
     try {
         const users = await User.find({})
-        res.render('users/allUsers', { users })
+        res.render('users/allUsers', { users, userInSession: req.session.currentUser })
     }
     catch (error) {
         console.log(error)
     }
 }
 
-
 const deleteUser = async (req, res) => {
     try {
         const { userId } = req.body
         await User.findByIdAndDelete(userId)
         console.log("User was deleted succesfully")
-        res.redirect('users/allUsers')
+        res.redirect('/users')
     }
     catch (error) {
         console.log(error)
