9ecaa3fb35af7134e88dc75815594f6fc17b1b8e
Simon Björkberg <127056123+SimonBjorkberg@users.noreply.github.com>
2023-06-29 12:26:56 +0200

    route guarding for threads fully complete

--- middleware/route-guard.js
+++ middleware/route-guard.js
@@ -1,3 +1,25 @@
+const User = require("../models/User.model");
+const Thread = require("../models/Thread.model");
+const Post = require("../models/Post.model");
+const { el } = require("date-fns/locale");
+
+const isThreadAuthor = async (req, res, next) => {
+  try {
+    const { threadId } = req.params;
+    const thread = await Thread.findById(threadId).populate("author");
+    if (
+      thread.author.username === req.session.currentUser.username ||
+      req.session.currentUser.role === "admin"
+    ) {
+      next();
+    } else {
+      return res.redirect(`/threads/${threadId}`)
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const isLoggedIn = (req, res, next) => {
   if (!req.session.currentUser) {
     return res.redirect("/login");
@@ -13,6 +35,7 @@ const isLoggedOut = (req, res, next) => {
 };
 
 module.exports = {
+  isThreadAuthor,
   isLoggedIn,
   isLoggedOut,
 };
--- routes/thread-routes.js
+++ routes/thread-routes.js
@@ -1,11 +1,12 @@
 const threadController = require("../controllers/threadController");
+const { isThreadAuthor, isLoggedIn, isLoggedOut } = require('../middleware/route-guard')
 const express = require("express");
 const router = express.Router();
 
 router.get("/:threadId", threadController.getThread);
 router.post("/create", threadController.createThread);
-router.get('/:threadId/edit', threadController.editThread)
-router.post("/:threadId/delete", threadController.deleteThread);
-router.post('/:threadId/update', threadController.updateThread);
+router.get('/:threadId/edit', isThreadAuthor, threadController.editThread)
+router.post("/:threadId/delete", isThreadAuthor, threadController.deleteThread);
+router.post('/:threadId/update', isThreadAuthor, threadController.updateThread);
 
 module.exports = router;
