318fff9d197b4db6ff7c2dce04c6981776f6c57f
Simon Björkberg <127056123+SimonBjorkberg@users.noreply.github.com>
2023-07-02 03:07:44 +0200

    profile page improved

--- public/js/script.js
+++ public/js/script.js
@@ -7,27 +7,25 @@ function threadClick(threadId) {
   window.location.href = "/threads/" + threadId;
 }
 
-const signupButton = document.getElementById('signup-modal-button')
-const signupModal = document.getElementById('signupModal')
-const signupCloseButton = document.getElementById('signup-close-modal')
+const signupButton = document.getElementById("signup-modal-button");
+const signupModal = document.getElementById("signupModal");
+const signupCloseButton = document.getElementById("signup-close-modal");
 
-const loginButton = document.getElementById('login-modal-button')
-const loginModal = document.getElementById('loginModal')
-const loginCloseButton = document.getElementById('login-close-modal')
+const loginButton = document.getElementById("login-modal-button");
+const loginModal = document.getElementById("loginModal");
+const loginCloseButton = document.getElementById("login-close-modal");
 
 loginButton.addEventListener("click", () => {
   loginModal.showModal();
 });
-signupButton.addEventListener('click', () => {
-  signupModal.showModal()
-})
+signupButton.addEventListener("click", () => {
+  signupModal.showModal();
+});
 
-loginCloseButton.addEventListener('click', () => {
-  loginModal.close()
-})
+loginCloseButton.addEventListener("click", () => {
+  loginModal.close();
+});
 
-signupCloseButton.addEventListener('click', () => {
-  signupModal.close()
-})
-
-
+signupCloseButton.addEventListener("click", () => {
+  signupModal.close();
+});
