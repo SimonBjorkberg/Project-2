2e4a07c362c3856f41486822c7ee8505d7954588
Achref LAABIDI <abidi.achref95@gmail.com>
2023-07-06 20:00:43 +0100

    updated the inbox guard

--- middleware/inbox-guard.js
+++ middleware/inbox-guard.js
@@ -1,6 +1,8 @@
 const inboxGuard = (req, res, next) => {
   if (req.session.currentUser) {
     next();
+  } else if (!req.session.currentUser) {
+    return res.redirect('/')
   } else {
     res.redirect("/error");
   }
